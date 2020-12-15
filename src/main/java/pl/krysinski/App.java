abbdcca4b974f25b37a9c22ae03f7022ee4d1c40
Marcin <marcin.jakubkrysinski@gmail.com>
2020-12-16 00:24:45 +0100

    refactoring reader to returned users list

--- src/main/java/pl/krysinski/App.java
+++ src/main/java/pl/krysinski/App.java
@@ -1,14 +1,15 @@
 package pl.krysinski;
 
+import pl.krysinski.model.User;
+
 import java.io.IOException;
 import java.util.List;
 
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
+public class App {
+    public static void main(String[] args) throws IOException {
         Reader reader = new Reader();
-        List<String[]> usersList;
+        List<User> usersList;
         usersList = reader.readData();
         System.out.print(usersList);
     }
--- src/main/java/pl/krysinski/Reader.java
+++ src/main/java/pl/krysinski/Reader.java
@@ -1,5 +1,8 @@
 package pl.krysinski;
 
+import pl.krysinski.model.Gender;
+import pl.krysinski.model.User;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,20 +10,49 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Reader {
 
-    public List<String[]> readData() throws IOException {
+    public List<User> readData() throws IOException {
         String file = "src/main/java/pl/krysinski/USERS_DATA.csv";
-        List<String[]> content = new ArrayList<>();
-        try(BufferedReader br = new BufferedReader(new
-                FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                content.add(line.split(","));
+        List<User> content = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String[] headers = br.readLine().split(",");
+            String record;
+            while ((record = br.readLine()) != null) {
+                String[] tokens = record.split(",");
+                User user = createUser(headers, tokens);
+                content.add(user);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return content;
+    }
+
+    private User createUser(String[] headers, String[] tokens) {
+        User user = new User();
+        for (int h = 0; h < headers.length; h++) {
+            if ("id".equals(headers[h])) {
+                user.setId(tokens[h]);
+            }
+            if ("first_name".equals(headers[h])) {
+                user.setFirst_name(tokens[h]);
+            }
+            if ("last_name".equals(headers[h])) {
+                user.setLast_name(tokens[h]);
+            }
+            if ("email".equals(headers[h])) {
+                user.setEmail(tokens[h]);
+            }
+            if ("gender".equals(headers[h])) {
+                user.setGender(Gender.valueOf(tokens[h]));
+            }
+            if ("ip_address;;".equals(headers[h])) {
+                String ip_address = tokens[h];
+                user.setIp_address(ip_address.substring(0, ip_address.length() - 2));
+            }
+        }
+        return user;
     }
 }
